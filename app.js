df61f382d053e78e9c925d12390db189857a7f21
mugala <mugalalevi21@gmail.com>
2018-08-14 13:32:38 +0300

    xhr object This commit will create the xhr object that will initialize the GET process from the url asynchronously

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ document.querySelector('.get-jokes').addEventListener('click', getJokes);
 function getJokes(e){
   const number = document.querySelector('input[type = "number"]').value;
 
-  console.log(number);
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET',`http://api.icndb.com/jokes/random/${number}`,true);
+
+
+  xhr.send();
   e.preventDefault();
 }
