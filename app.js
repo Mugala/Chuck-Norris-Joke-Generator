a7b2bc634d8410b801c18e9237e0d1eab5e1ee20
mugala <mugalalevi21@gmail.com>
2018-08-14 13:28:14 +0300

    Getting input value This commit will grab the number from the input field and get the value

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ document.querySelector('.get-jokes').addEventListener('click', getJokes);
 
 
 function getJokes(e){
-  console.log('get jokes');
+  const number = document.querySelector('input[type = "number"]').value;
 
+  console.log(number);
   e.preventDefault();
 }
